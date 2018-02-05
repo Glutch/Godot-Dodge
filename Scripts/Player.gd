7f8f807676f8012a3725e0398d2c46dd62fd156a
Glutch <oliverwiljohansson@gmail.com>
2018-02-05 22:53:47 +0100

    added movement and animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,17 +8,27 @@ func _ready():
 	screenSize = get_viewport_rect().size
 	
 func _process(delta):
+	move(delta)
+	
+func move(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_down"):
+		$AnimatedSprite.flip_v = true
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
+		$AnimatedSprite.flip_v = false
 		velocity.y -= 1
 	if velocity.length() > 0:
+		$AnimatedSprite.play()
 		velocity = velocity.normalized() * SPEED
+	else:
+		$AnimatedSprite.stop()
 		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screenSize.x)
